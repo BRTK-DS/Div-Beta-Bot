e920cf76771ca5e020c410fa5ba35290273ac177
BRTK-DS <150911484+BRTK-DS@users.noreply.github.com>
2024-03-11 15:54:20 +0100

    Automod (#9)
    * added automod cog
    
    * Added automod
    
    * aktualizacja automoda
    
    * fixed code in automod

---
+++ cogs/automod.py
@@ -0,0 +1,38 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Guild
+import os
+from emoji import *
+    
+class automod(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+        with open('banned_words.txt', 'r') as file:
+            self.banned_words = [word.strip()[2:].lower() for word in file.readlines()]
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        
+        if message.author.bot:
+            return
+        
+        content_lower = message.content.lower()
+        for word in self.banned_words:
+            if word in content_lower:
+                muted_role = message.guild.get_role(1216727135142805585)
+                role_id = message.guild.get_role(1038194522422775878)
+                
+                try:
+                    appeal_message = f'{failed_emoji} Zostałeś wyciszony na serwerze za użycie niedozwolonych wyrazów. Jeśli chcesz odwołać się od wyciszenia, możesz to zrobić na odpowiednim kanale.'
+                    await message.author.send(appeal_message)
+                except nextcord.HTTPException:
+                    pass
+                await message.author.add_roles(muted_role)
+                await message.author.remove_roles(role_id)
+                await message.channel.send(f"{failed_emoji} {message.author.mention} został wyciszony.")
+                
+                break
+            
+def setup(client):
+    client.add_cog(automod(client))
