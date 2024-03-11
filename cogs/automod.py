b7b9fd3aa897170dc21498951a178c0c1cf128d3
BRTK-DS <bdsiadlak@gmail.com>
2024-03-11 13:24:56 +0100

    Added automod

--- cogs/automod.py
+++ cogs/automod.py
@@ -1,17 +1,27 @@
 import nextcord
 from nextcord.ext import commands
-from nextcord import Interaction, Guild
-import os
-
+    
 class automod(commands.Cog):
     def __init__(self, client):
         self.client = client
         
-    commands.Cog.listener()
+        with open('banned_words.txt', 'r') as file:
+            self.banned_words = [word.strip().lower() for word in file.readlines()]
+        
+    @commands.Cog.listener()
     async def on_message(self, message):
         
-        guild_id = 1038037955836661840
-        if message.guild.id != guild_id:
+        if message.author.bot:
             return
         
-        
+        content_lower = message.content.lower()
+        for word in self.banned_words:
+            if word in content_lower:
+                muted_role = nextcord.utils.get(message.guild.roles, name='Muted')
+                
+                await message.author.add_roles(muted_role)
+                await message.channel.send(f"{message.author.mention} został wyciszony")
+                break
+            
+def setup(client):
+    client.add_cog(automod(client))
